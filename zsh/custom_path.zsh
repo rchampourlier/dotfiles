3e978965a5bee7846a489e4d2fa9a4e5e57f0999
Romain Champourlier <pro@rchampourlier.com>
2015-02-06 23:06:53 +0100

    Added some customization
    (cherry picked from commit d14b6b23ff7ee28ac8136856d1349ce4cc81a70c)

---
+++ zsh/custom_path.zsh
@@ -0,0 +1,5 @@
+export PATH=$PATH:/usr/local/share/npm/bin # For NPM executables
+export PATH=$PATH:$HOME/.rvm/bin # Add RVM to PATH for scripting
+export PATH="/usr/local/heroku/bin:$PATH" # Added by the Heroku Toolbelt
+export PATH="./node_modules/.bin:$PATH"
+
